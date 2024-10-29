b680a864292af1c67e241d33202d6276be17558b
aldoushux503 <elnikovnikita4@gmail.com>
2024-10-29 17:50:26 +0100

    Complete first homework task

---
+++ src/HelloName.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class HelloName {
+    public static void main(String[] args) {
+        String info = args.length > 0 ? String.join(" ", Arrays.asList(args)) : "World";
+        System.out.printf("Hello %s%n", info);
+    }
+}
