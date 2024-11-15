3f22d418af2ad019bc54647828d0022bf9f77610
aldoushux503 <elnikovnikita4@gmail.com>
2024-11-15 18:12:06 +0100

    Add a final Constants class

---
+++ src/main/java/com/labas/travelagency/util/Constants.java
@@ -0,0 +1,9 @@
+package main.java.com.labas.travelagency.util;
+
+public final class Constants {
+    public static final double DEFAULT_TOUR_TAX = 1.15;
+
+    static {
+        System.out.println("Constants class loaded with default tax: " + DEFAULT_TOUR_TAX);
+    }
+}
