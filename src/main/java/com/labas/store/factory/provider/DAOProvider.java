2c5becbee6dcba9793b803f3c4f26a3c5979341f
aldoushux503 <elnikovnikita4@gmail.com>
2025-02-07 20:24:28 +0100

    Replace the long if-else in the DAOProvider class with a switch statement

--- src/main/java/com/labas/store/factory/provider/DAOProvider.java
+++ src/main/java/com/labas/store/factory/provider/DAOProvider.java
@@ -20,40 +20,30 @@ public class DAOProvider {
     }
 
     private <T> T createDAO(Class<T> daoClass) {
-        if (daoClass == IUserDAO.class) {
-            return (T) daoFactory.createUserDAO();
-        } else if (daoClass == IOrderStatusDAO.class) {
-            return (T) daoFactory.createOrderStatusDAO();
-        } else if (daoClass == IPaymentMethodDAO.class) {
-            return (T) daoFactory.createPaymentMethodDAO();
-        } else if (daoClass == ICarrierDAO.class) {
-            return (T) daoFactory.createCarrierDAO();
-        } else if (daoClass == IShippingStatusDAO.class) {
-            return (T) daoFactory.createShippingStatusDAO();
-        } else if (daoClass == ICategoryDAO.class) {
-            return (T) daoFactory.createCategoryDAO();
-        } else if (daoClass == IProductDAO.class) {
-            return (T) daoFactory.createProductDAO();
-        } else if (daoClass == IRoleDAO.class) {
-            return (T) daoFactory.createRoleDAO();
-        } else if (daoClass == IAddressDAO.class) {
-            return (T) daoFactory.createAddressDAO(getDAO(IUserDAO.class));
-        } else if (daoClass == IOrderDAO.class) {
-            return (T) daoFactory.createOrderDAO(getDAO(IOrderStatusDAO.class), getDAO(IUserDAO.class));
-        } else if (daoClass == IPaymentDAO.class) {
-            return (T) daoFactory.createPaymentDAO(
+        return switch (daoClass.getSimpleName()) {
+            case "IUserDAO" -> (T) daoFactory.createUserDAO();
+            case "IOrderStatusDAO" -> (T) daoFactory.createOrderStatusDAO();
+            case "IPaymentMethodDAO" -> (T) daoFactory.createPaymentMethodDAO();
+            case "ICarrierDAO" -> (T) daoFactory.createCarrierDAO();
+            case "IShippingStatusDAO" -> (T) daoFactory.createShippingStatusDAO();
+            case "ICategoryDAO" -> (T) daoFactory.createCategoryDAO();
+            case "IProductDAO" -> (T) daoFactory.createProductDAO();
+            case "IRoleDAO" -> (T) daoFactory.createRoleDAO();
+            case "IAddressDAO" -> (T) daoFactory.createAddressDAO(getDAO(IUserDAO.class));
+            case "IOrderDAO" -> (T) daoFactory.createOrderDAO(getDAO(IOrderStatusDAO.class), getDAO(IUserDAO.class));
+            case "IPaymentDAO" -> (T) daoFactory.createPaymentDAO(
                     getDAO(IPaymentMethodDAO.class),
                     getDAO(IOrderDAO.class),
                     getDAO(IUserDAO.class)
             );
-        } else if (daoClass == IShippingDAO.class) {
-            return (T) daoFactory.createShippingDAO(
+            case "IShippingDAO" -> (T) daoFactory.createShippingDAO(
                     getDAO(IShippingStatusDAO.class),
                     getDAO(IOrderDAO.class),
                     getDAO(IAddressDAO.class),
                     getDAO(ICarrierDAO.class)
             );
-        }
+            default -> throw new IllegalArgumentException("Unsupported DAO class: " + daoClass);
+        };
         throw new IllegalArgumentException("Unknown DAO class: " + daoClass.getSimpleName());
     }
 }
